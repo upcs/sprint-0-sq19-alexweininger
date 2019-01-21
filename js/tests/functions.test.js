19cf3ee4a1e2fba109dd4ac366ed4fc84ae4b5f0
alexweininger <alex.weininger@live.com>
2019-01-20 17:51:34 -0800

    added a failing test

--- js/tests/functions.test.js
+++ js/tests/functions.test.js
@@ -9,3 +9,8 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + -1 to equal 0', () => {
 	expect(sum(1, -1)).toBe(0);
 })
+
+// purposely failing test
+test('adds 1 and 1 to equal 4', () => {
+	expect(sum(1, 1)).toBe(4);
+})
