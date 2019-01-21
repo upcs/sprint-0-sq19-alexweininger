f4b61a341ff57699877670d234010d50fb73f27b
alexweininger <alex.weininger@live.com>
2019-01-20 16:30:08 -0800

    improved tests

--- js/tests/functions.test.js
+++ js/tests/functions.test.js
@@ -1,9 +1,11 @@
 const sum = require('../functions');
 
+// test 1
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+// test 2
 test('adds 1 + -1 to equal 0', () => {
 	expect(sum(1, -1)).toBe(0);
 })
